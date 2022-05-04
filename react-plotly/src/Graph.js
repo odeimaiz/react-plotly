0f82f41a69fed33c79db419674010cfdbf8df95e
odeimaiz <maiz@itis.swiss>
2022-05-04 14:29:04 +0200

    A sine

--- react-plotly/src/Graph.js
+++ react-plotly/src/Graph.js
@@ -3,19 +3,32 @@ import Plot from 'react-plotly.js';
 
 class Graph extends React.Component {
   render() {
+    const data = {
+      x: [],
+      y: []
+    };
+    const n = 1000;
+    for (let i = 0; i < n; i++) {
+      const t = i / (n - 1) * 2 - 1;
+
+      // A sine wave:
+      data.x[i] = t * Math.PI;
+      data.y[i] = Math.sin(t * Math.PI);
+    }
+
     return (
       <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
+        data={
+          [{
+            x: data.x,
+            y: data.y,
             type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={{width: 640, height: 480, title: 'A Fancy Plot'}}
+            marker: {
+              color: 'red'
+            },
+          }]
+        }
+        layout={{width: 640, height: 480, title: 'A Sine'}}
       />
     );
   }

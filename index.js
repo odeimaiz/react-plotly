ccc7cb6c821cd5f55913bc748e800a32703eac28
odeimaiz <maiz@itis.swiss>
2022-05-04 15:49:19 +0200

    working server after build

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const path = require("path");
 const express = require("express");
 const app = express(); // create express app
 
+app.use(express.static(path.join(__dirname, "react-plotly", "build")));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "react-plotly", "public", "index.html"));
 });
